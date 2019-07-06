93ace7cdf0dffe4cb7aa5fdf17dfb28f8aa97590
Victor R <victor.rocha@mercadolivre.com>
2019-07-06 18:40:58 -0300

    implemented library inside test app

--- app/src/main/java/com/stonetree/paymentsapp/MainActivity.kt
+++ app/src/main/java/com/stonetree/paymentsapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.stonetree.paymentsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.stonetree.payments.Payments
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Payments.start(this)
     }
 }
---
+++ payments/src/main/java/com/stonetree/payments/Payments.kt
@@ -0,0 +1,13 @@
+package com.stonetree.payments
+
+import android.content.Context
+
+class Payments {
+
+    companion object {
+        fun start(context: Context) {
+
+        }
+    }
+
+}
