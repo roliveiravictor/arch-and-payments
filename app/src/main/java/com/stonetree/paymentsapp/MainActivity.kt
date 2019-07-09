f63f7c95be50f8eb6b1c27303783ff3deca42cc0
Victor R <victor.rocha@mercadolivre.com>
2019-07-09 20:36:53 -0300

    reduced app example architecture

--- app/src/main/java/com/stonetree/paymentsapp/MainActivity.kt
+++ app/src/main/java/com/stonetree/paymentsapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.stonetree.paymentsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.stonetree.payments.Payments
+import com.stonetree.payments.listeners.PaymentsListener
 import com.stonetree.paymentsapp.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PaymentsListener {
+
+    private var vm: MainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,10 +22,30 @@ class MainActivity : AppCompatActivity() {
             this, R.layout.activity_main)
 
         bindData(container)
+        bindObserver()
     }
 
     private fun bindData(container: ActivityMainBinding) {
-        container.presenter = MainPresenter(this)
         container.vm = MainViewModel()
+        container.view = this@MainActivity
+    }
+
+    private fun bindObserver() {
+        val purchase = Observer<String> {
+            purchase.text = it
+        }
+        vm.status.observe(this, purchase)
+    }
+
+    override fun onPurchaseFailure() {
+        vm.status.value = "Product not found"
+    }
+
+    override fun onPurchased() {
+        vm.status.value = "Purchased"
+    }
+
+    fun onClick(view: View) {
+        Payments.start(view.context, this)
     }
 }
